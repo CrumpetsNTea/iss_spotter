ca0a4c8e2854e0c52989db18169f32d8511ae2be
CrumpetsNTea <t.botell@gmail.com>
2021-11-04 18:08:51 -0400

    working fetchMyIP function that uses a callback and request function to get IP - includes error fresponse if the response.statusCode is not 200. index calls this function correctly and returns IP address

--- iss.js
+++ iss.js
@@ -1,1 +1,22 @@
 //contain the logic for fetching data from each API
+const request = require('request');
+
+
+const fetchMyIP = (callback) => {
+  request(`https://api.ipify.org/?format=json`, (error, response, body) => {
+
+    if (error) {
+      callback(error, null);
+      return;
+    }
+    if (response.statusCode !== 200) {
+      const msg = `Status Codee ${response.statusCode} when fetching I{. Response: ${body}`;
+      callback(Error(msg), null); //Error here creates a new Error object that we can pass around
+      return;
+    }
+    let IP = body;
+    callback(null, IP);
+  });
+};
+
+module.exports = { fetchMyIP };

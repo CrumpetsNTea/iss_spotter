8ced18da9bf2a06ae8a76983d255a2dd3acb89ef
CrumpetsNTea <t.botell@gmail.com>
2021-11-04 20:11:34 -0400

    now have working fetchCoords function.

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 //require and run our main fetch function
 
-const { fetchMyIP } = require("./iss");
+const { fetchMyIP, fetchCoordsByIP } = require("./iss");
 
 fetchMyIP((error, ip) => {
   if (error) {
@@ -8,5 +8,12 @@ fetchMyIP((error, ip) => {
     return;
   }
   console.log("It worked! Returned IP:", ip);
+});
 
+fetchCoordsByIP('23.239.32.57', (error, coordinates) => {
+  if (error) {
+    console.log("It didn't work!", error);
+    return;
+  }
+  console.log("It worked! Returned Coordinates:", coordinates);
 });
--- iss.js
+++ iss.js
@@ -19,4 +19,21 @@ const fetchMyIP = (callback) => {
   });
 };
 
-module.exports = { fetchMyIP };
+const fetchCoordsByIP = (ip, callback) => {
+  request(`https://freegeoip.app/json/${ip}`, (error, response, body) => {
+    if (error) {
+      callback(error, null);
+      return;
+    }
+    if (response.statusCode !== 200) {
+      callback(Error(`Status Code ${response.statusCode} when fetching Coordinates for IP: ${body}`), null);
+      return;
+    }
+    const { latitude, longitude } = JSON.parse(body);
+    callback(null, { latitude, longitude });
+    return;
+  });
+};
+
+
+module.exports = { fetchMyIP, fetchCoordsByIP };

9dc4a0f9ded72e0f320d8bb0c2cb28d0f76fe99b
CrumpetsNTea <t.botell@gmail.com>
2021-11-04 21:47:36 -0400

    said changes not staged

--- iss_promised.js
+++ iss_promised.js
@@ -1,7 +1,7 @@
 const request = require('request-promise-native');
 
 const fetchMyIP = () => { //function that grabs an ip that has a callback parameter
-  return request(`https://api.ipify.org/?format=json`)
+  return request(`https://api.ipify.org/?format=json`);
 };
 
 module.exports = { fetchMyIP };

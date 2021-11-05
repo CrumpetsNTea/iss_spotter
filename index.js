8c2e6789b6ca93fd71e0fc72a819d17a82ad541b
CrumpetsNTea <t.botell@gmail.com>
2021-11-04 21:12:14 -0400

    working nextISSTimes function - outputs correctly - tested and nested in index.js.

--- index.js
+++ index.js
@@ -2,32 +2,40 @@
 
 const { fetchMyIP, fetchCoordsByIP, fetchISSFlyOverTimes, nextISSTimesForMyLocation } = require("./iss");
 
-nextISSTimesForMyLocation((error, passTimes) => {
+fetchMyIP((error, ip) => {
   if (error) {
-    return console.log("It didn't work!", error);
+    console.log("It didn't work!", error);
+    return;
   }
-  fetchMyIP((error, ip) => {
+  console.log("It worked! Returned IP:", ip);
+  
+  fetchCoordsByIP(ip, (error, coordinates) => {
     if (error) {
       console.log("It didn't work!", error);
       return;
     }
-    console.log("It worked! Returned IP:", ip);
-  
-    fetchCoordsByIP(ip, (error, coordinates) => {
+    console.log("It worked! Returned Coordinates:", coordinates);
+    
+    fetchISSFlyOverTimes({ latitude: 44.5698, longitude: -80.9439 }, (error, pass) => {
       if (error) {
         console.log("It didn't work!", error);
         return;
       }
-      console.log("It worked! Returned Coordinates:", coordinates);
-    
-      fetchISSFlyOverTimes({ latitude: 44.5698, longitude: -80.9439 }, (error, pass) => {
-        if (error) {
-          console.log("It didn't work!", error);
-          return;
+      console.log("It worked! Returned Passes:", pass);
+      const printPassTimes = (passTimes) => {
+        for (const pass of passTimes) {
+          const datetime = new Date(0);
+          datetime.setUTCSeconds(pass.risetime);
+          const duration = pass.duration;
+          console.log(`Next pass at ${datetime} for ${duration} seconds!`);
         }
-        console.log("It worked! Returned Passes:", pass);
+      };
+      nextISSTimesForMyLocation((error, passTimes) => {
+        if (error) {
+          return console.log("It didn't work!", error);
+        }
         // success, print out the deets!
-        console.log(passTimes);
+        printPassTimes(passTimes);
       });
     });
   });
@@ -35,7 +43,6 @@ nextISSTimesForMyLocation((error, passTimes) => {
 
 
 
- 
 
 
 
--- iss.js
+++ iss.js
@@ -49,7 +49,22 @@ const fetchISSFlyOverTimes = function(coords, callback) {
 };
 
 const nextISSTimesForMyLocation = (callback) => {
-
+  fetchMyIP((error, ip) => { //first step
+    if (error) {
+      return callback(error, null);
+    }
+    fetchCoordsByIP(ip, (error, loc) => {
+      if (error) {
+        return callback(error, null);
+      }
+      fetchISSFlyOverTimes(loc, (error, nextPasses) => {
+        if (error) {
+          return callback(error, null);
+        }
+        callback(null, nextPasses);
+      });
+    });
+  });
 };
 
 module.exports = { fetchMyIP, fetchCoordsByIP, fetchISSFlyOverTimes, nextISSTimesForMyLocation };
